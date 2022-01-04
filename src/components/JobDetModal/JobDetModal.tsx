4eddf521f069788fa353586c17665d8b39440592
Antonios Zachos <55564849+antoniozah@users.noreply.github.com>
2022-01-04 02:43:36 +0200

    refactor JobDetModal & adding JobDetails Component

--- src/components/JobDetModal/JobDetModal.tsx
+++ src/components/JobDetModal/JobDetModal.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { IJobDetails, IJobList } from '../../interfaces';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import JobCardItem from '../JobCardItem/JobCardItem';
+
 import './JobDetModal.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleModal } from '../../features/modalStatusSlice';
+import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
-import { useNavigate } from 'react-router-dom';
+import JobDetails from '../JobDetails/JobDetails';
 
 interface JobDetModalProps {}
 
 const JobDetModal = (props: JobDetModalProps) => {
-    const job = useSelector(
-        (state: RootState) => state.jobDetails.fetchedJobDetails
-    );
-
-    const modalStatus = useSelector(
-        (state: RootState) => state.modalStatus.value
-    );
-
     const hasError = useSelector(
         (state: RootState) => state.modalHandling.modalError.hasError
     );
@@ -26,22 +17,6 @@ const JobDetModal = (props: JobDetModalProps) => {
     const errorMessage = useSelector(
         (state: RootState) => state.modalHandling.modalError.errorMessage
     );
-
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const handleCloseModal = () => {
-        dispatch(toggleModal());
-        document.body.classList.remove('modal-open');
-    };
-
-    const handleApplicationBtn = () => {
-        navigate(`/success-application/${job.title}?${job.id}`);
-        if (modalStatus) {
-            dispatch(toggleModal());
-            document.body.classList.remove('modal-open');
-        }
-    };
 
     return (
         <div className="modal">
@@ -53,37 +28,7 @@ const JobDetModal = (props: JobDetModalProps) => {
                 {hasError ? (
                     <ErrorMessage errorMessage={errorMessage} />
                 ) : (
-                    <>
-                        <div className="modal__header">
-                            <h3 className="modal__title">Apply for the Job</h3>
-                            {!errorMessage?.hasBtn && (
-                                <span
-                                    className="modal__close"
-                                    onClick={handleCloseModal}
-                                >
-                                    X
-                                </span>
-                            )}
-                        </div>
-
-                        <div className="modal__card-item">
-                            <JobCardItem jobData={job} />
-                        </div>
-                        <div
-                            className="modal__description"
-                            dangerouslySetInnerHTML={{
-                                __html: `${job.description}`,
-                            }}
-                        />
-                        <div className="modal__button">
-                            <button
-                                className="btn btn--big"
-                                onClick={handleApplicationBtn}
-                            >
-                                Send Application
-                            </button>
-                        </div>
-                    </>
+                    <JobDetails hasCloseBtn={errorMessage.hasBtn} />
                 )}
             </div>
         </div>
---
+++ src/components/JobDetails/JobDetails.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import './JobDetails.css';
+import JobCardItem from '../JobCardItem/JobCardItem';
+import { RootState } from '../../app/store';
+import { toggleModal } from '../../features/modalStatusSlice';
+
+interface JobDetailsProps {
+    hasCloseBtn?: boolean;
+}
+
+const JobDetails = (props: JobDetailsProps) => {
+    const job = useSelector(
+        (state: RootState) => state.jobDetails.fetchedJobDetails
+    );
+
+    const modalStatus = useSelector(
+        (state: RootState) => state.modalStatus.value
+    );
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleCloseModal = () => {
+        dispatch(toggleModal());
+        document.body.classList.remove('modal-open');
+    };
+
+    const handleApplicationBtn = () => {
+        navigate(`/success-application/${job.title}?${job.id}`);
+        if (modalStatus) {
+            dispatch(toggleModal());
+            document.body.classList.remove('modal-open');
+        }
+    };
+    return (
+        <>
+            <div className="modal__header">
+                <h3 className="modal__title">Apply for the Job</h3>
+                {!props.hasCloseBtn && (
+                    <span className="modal__close" onClick={handleCloseModal}>
+                        X
+                    </span>
+                )}
+            </div>
+
+            <div className="modal__card-item">
+                <JobCardItem jobData={job} />
+            </div>
+            <div
+                className="modal__description"
+                dangerouslySetInnerHTML={{
+                    __html: `${job.description}`,
+                }}
+            />
+            <div className="modal__button">
+                <button className="btn btn--big" onClick={handleApplicationBtn}>
+                    Send Application
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default JobDetails;
